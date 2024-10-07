e0ac4a28eb6d60b9b9e16acee82918de852cd84a
Guillaume <guillaume.rot@gmail.com>
2024-10-07 22:58:54 +0200

    feat: background

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import { Layout } from "@/components/layout";
 
 export default function Home() {
   return (
-    <Layout>
-      <div className="ml-2">Test GUI</div>
-    </Layout>
+    <div className="bg-secondary">
+      <Layout>
+        <div className="ml-2">Test GUI</div>
+      </Layout>
+    </div>
   );
 }
