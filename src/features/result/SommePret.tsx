108d93b22790b1f5382f9bb09429ee5a94fad1c8
Guillaume <guillaume.rot@gmail.com>
2024-10-09 23:36:04 +0200

    feat: design somme pret

--- src/features/result/SommePret.tsx
+++ src/features/result/SommePret.tsx
@@ -5,19 +5,19 @@ export const SommePret = async () => {
   return (
     <Card className="border-2 rounded-xl py-8 w-full max-h-32">
       <CardContent className="grid gap-4 flex h-full">
-        <div className="w-1/3">
+        <div className="w-1/3 h-full">
           <CardTitle>Montant du prêt hors interêts</CardTitle>
-          <CardContent className="mt-4 text-2xl">190 000 €</CardContent>
+          <h4 className="mt-4 text-2xl">190 000 €</h4>
         </div>
         <Separator orientation="vertical" />
-        <div className="w-1/3">
+        <div className="w-1/3 h-full">
           <CardTitle>Frais bancaires</CardTitle>
-          <CardContent className="mt-4 text-2xl">57 000 €</CardContent>
+          <h4 className="mt-4 text-2xl">57 000 €</h4>
         </div>
         <Separator orientation="vertical" />
-        <div className="w-1/3">
+        <div className="w-1/3 h-full">
           <CardTitle>Valeur du prêt</CardTitle>
-          <CardContent className="mt-4 text-2xl">247 000 €</CardContent>
+          <h4 className="mt-4 text-2xl">247 000 €</h4>
         </div>
       </CardContent>
     </Card>
