2327098bc70f1305e7e0a0dd4a362803d7267af8
Guillaume <guillaume.rot@gmail.com>
2024-10-29 07:41:59 +0100

    feat: remove access section action

--- src/features/landing/AccessSection.tsx
+++ src/features/landing/AccessSection.tsx
@@ -1,33 +1,9 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useMutation } from "@tanstack/react-query";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
 import { IoArrowForwardCircle } from "react-icons/io5";
-import { z } from "zod";
-import { AccessSchema, AccessType } from "../simulateur/simulateur.schema";
-import { insertMail } from "./access.action";
 
-export type AccessSectionProps = {
-  defaultValues?: AccessType;
-};
-
-export const AccessSection = (props: AccessSectionProps) => {
-  const router = useRouter();
-
-  const form = useForm<z.infer<typeof AccessSchema>>({
-    resolver: zodResolver(AccessSchema),
-    defaultValues: props.defaultValues,
-  });
-
-  const mutation = useMutation({
-    mutationFn: async (values: AccessType) => {
-      await insertMail(values);
-    },
-  });
-
+export const AccessSection = () => {
   return (
     <div className="mx-auto w-fullw-1/2 px-8 py-4">
       <Link
