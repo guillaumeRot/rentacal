cb9cc2e97f72850046098d42e788eb0f57c444e2
Guillaume <guillaume.rot@gmail.com>
2024-10-29 21:38:39 +0100

    feat: add details rentabilites

--- src/features/simulateur/RentabiliteBrute.tsx
+++ src/features/simulateur/RentabiliteBrute.tsx
@@ -1,4 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import Link from "next/link";
+import { useState } from "react";
 import { IoTrendingUpOutline } from "react-icons/io5";
 import { PourcentageFormat } from "../PourcentageFormat";
 
@@ -7,15 +10,49 @@ export type RentabiliteBruteProps = {
 };
 
 export const RentabiliteBrute = (props: RentabiliteBruteProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Card className="rounded-3xl lg:w-2/4 max-h-32">
-      <CardHeader>
-        <CardTitle>Rentabilité brute</CardTitle>
-      </CardHeader>
-      <CardContent className="gap-4 text-2xl lg:text-4xl flex font-extrabold text-secondary-foreground">
-        <PourcentageFormat value={props.rentabiliteBrute} className="w-3/4" />
-        <IoTrendingUpOutline />
-      </CardContent>
-    </Card>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Card className="rounded-3xl lg:w-2/4 max-h-40">
+        <CardHeader>
+          <CardTitle>Rentabilité brute</CardTitle>
+        </CardHeader>
+        <CardContent className="gap-4 grid text-2xl lg:text-4xl font-extrabold text-secondary-foreground">
+          <div className="flex">
+            <PourcentageFormat
+              value={props.rentabiliteBrute}
+              className="w-3/4"
+            />
+            <IoTrendingUpOutline />
+          </div>
+          <DialogTrigger asChild>
+            <Link
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setOpen(true);
+              }}
+              className="text-xs text-black underline"
+            >
+              Plus de détails...
+            </Link>
+          </DialogTrigger>
+        </CardContent>
+      </Card>
+
+      <DialogContent className="sm:max-w-[425px] p-4">
+        <h2 className="text-lg font-bold">Rentabilité brute</h2>
+        <p>Règle de calcul:</p>
+        <p>R = loyers annuel / prix achat</p>
+
+        <button
+          onClick={() => setOpen(false)}
+          className="mt-4 px-4 py-2 bg-gray-300 rounded"
+        >
+          Fermer
+        </button>
+      </DialogContent>
+    </Dialog>
   );
 };
--- src/features/simulateur/RentabiliteNette.tsx
+++ src/features/simulateur/RentabiliteNette.tsx
@@ -1,4 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import Link from "next/link";
+import { useState } from "react";
 import { IoWalletOutline } from "react-icons/io5";
 import { PourcentageFormat } from "../PourcentageFormat";
 
@@ -7,15 +10,52 @@ export type RentabiliteNetteProps = {
 };
 
 export const RentabiliteNette = (props: RentabiliteNetteProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <Card className="rounded-3xl lg:w-2/4 max-h-32">
-      <CardHeader>
-        <CardTitle>Rentabilité Nette</CardTitle>
-      </CardHeader>
-      <CardContent className="grid gap-4 text-2xl lg:text-4xl flex font-extrabold text-secondary-foreground">
-        <PourcentageFormat value={props.rentabiliteNette} className="w-3/4" />
-        <IoWalletOutline />
-      </CardContent>
-    </Card>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Card className="rounded-3xl lg:w-2/4 max-h-40">
+        <CardHeader>
+          <CardTitle>Rentabilité Nette</CardTitle>
+        </CardHeader>
+        <CardContent className="grid gap-4 text-2xl lg:text-4xl font-extrabold text-secondary-foreground">
+          <div className="flex">
+            <PourcentageFormat
+              value={props.rentabiliteNette}
+              className="w-3/4"
+            />
+            <IoWalletOutline />
+          </div>
+          <DialogTrigger asChild>
+            <Link
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                setOpen(true);
+              }}
+              className="text-xs text-black underline"
+            >
+              Plus de détails...
+            </Link>
+          </DialogTrigger>
+        </CardContent>
+      </Card>
+
+      <DialogContent className="sm:max-w-[425px] p-4">
+        <h2 className="text-lg font-bold">Rentabilité nette</h2>
+        <p>Règle de calcul:</p>
+        <p>
+          R = (loyers annuel - frais notaires - impots foncier - charges copro)
+          / prix achat
+        </p>
+
+        <button
+          onClick={() => setOpen(false)}
+          className="mt-4 px-4 py-2 bg-gray-300 rounded"
+        >
+          Fermer
+        </button>
+      </DialogContent>
+    </Dialog>
   );
 };
