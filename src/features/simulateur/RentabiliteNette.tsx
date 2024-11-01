a8f0d5861bb41dbb5114a62b8de1de5b02afdaa4
Guillaume <guillaume.rot@gmail.com>
2024-11-01 09:28:59 +0100

    feat: dialog rentabilite nette

--- src/features/simulateur/RentabiliteNette.tsx
+++ src/features/simulateur/RentabiliteNette.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { DialogTitle } from "@radix-ui/react-dialog";
+import { MathJax } from "better-react-mathjax";
 import Link from "next/link";
 import { useState } from "react";
 import { BsQuestionCircleFill } from "react-icons/bs";
@@ -54,13 +56,51 @@ export const RentabiliteNette = (props: RentabiliteNetteProps) => {
         </CardContent>
       </Card>
 
-      <DialogContent className="sm:max-w-[425px] p-4">
+      {/* <DialogContent className="sm:max-w-[425px] p-4">
         <h2 className="text-lg font-bold">Rentabilité nette</h2>
         <p>Règle de calcul:</p>
         <p>
           R = (loyers annuel - frais notaires - impots foncier - charges copro)
           x 100 / prix achat
         </p>
+
+        <button
+          onClick={() => setOpen(false)}
+          className="mt-4 px-4 py-2 bg-gray-300 rounded"
+        >
+          Fermer
+        </button>
+      </DialogContent> */}
+
+      <DialogContent className="sm:max-w-[425px] p-4 text-justify">
+        <DialogTitle className="text-lg font-bold">
+          Rentabilité nette
+        </DialogTitle>
+        <p>
+          La rentabilité nette d’un investissement immobilier locatif prend en
+          compte non seulement les revenus locatifs, mais aussi les charges et
+          les frais associés à la gestion du bien. Voici la formule :
+        </p>
+        <MathJax className="text-xl mx-auto">
+          {
+            "\\( \\text{R} = \\frac{(\\text{Loyer annuel} - \\text{Charges annuelles}) \\times 100}{\\text{Prix d'achat du bien + Frais d'acquisition}} \\)"
+          }
+        </MathJax>
+
+        <p>Où :</p>
+        <ul className="list-disc pl-4 pt-2">
+          <li>Loyer annuel : le total des loyers perçus sur une année.</li>
+          <li>
+            Charges annuelles : toutes les charges déductibles, comme les
+            charges de copropriété, les frais de gestion locative, les
+            assurances, les impôts fonciers, et les coûts d’entretien.
+          </li>
+          <li>
+            Prix d'achat du bien + Frais d'acquisition : le coût d’acquisition
+            total, incluant le prix d’achat, les frais de notaire, et les
+            éventuels frais d’agence.
+          </li>
+        </ul>
 
         <button
           onClick={() => setOpen(false)}
