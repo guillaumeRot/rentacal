bcba95d65de2935b0ad9a8e11f48208569992ffc
Guillaume <guillaume.rot@gmail.com>
2024-11-05 22:28:04 +0100

    feat: google analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/features/theme/ThemeProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MathJaxContext } from "better-react-mathjax";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 import { PropsWithChildren } from "react";
 
 const queryClient = new QueryClient();
@@ -12,18 +13,21 @@ export type ProvidersProps = PropsWithChildren;
 
 export const Providers = (props: ProvidersProps) => {
   return (
-    // <SessionProvider>
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <QueryClientProvider client={queryClient}>
-        <Toaster />
-        <MathJaxContext>{props.children}</MathJaxContext>
-      </QueryClientProvider>
-    </ThemeProvider>
-    // </SessionProvider>
+    <>
+      <GoogleAnalytics trackPageViews />
+      {/* <SessionProvider> */}
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <QueryClientProvider client={queryClient}>
+          <Toaster />
+          <MathJaxContext>{props.children}</MathJaxContext>
+        </QueryClientProvider>
+      </ThemeProvider>
+      {/* </SessionProvider> */}
+    </>
   );
 };
