417b4fdb74bc3af2510db1321890a94e94783c95
Guillaume <guillaume.rot@gmail.com>
2024-12-05 20:58:57 +0100

    feat: image gmail

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["media.licdn.com"], // Ajoutez ici les domaines externes nécessaires
+    domains: ["media.licdn.com", "lh3.googleusercontent.com"], // Ajoutez ici les domaines externes nécessaires
   },
 };
 
