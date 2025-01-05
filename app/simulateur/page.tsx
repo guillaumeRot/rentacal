7f0c2dc3ac53fe2f4522a6118f2087ead4f9c97e
Guillaume <guillaume.rot@gmail.com>
2025-01-05 15:43:01 +0100

    feat: allow free simulateur access

--- app/simulateur/page.tsx
+++ app/simulateur/page.tsx
@@ -7,24 +7,19 @@ import { RentaCalSidebar } from "@/features/RentaCalSidebar";
 import SimulationResult from "@/features/simulateur/SimulationResult";
 import CookieConsent from "@/features/theme/CookieConsent";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 export default function Result() {
   const session = useSession();
 
-  if (!session.data?.user) {
-    redirect(`/`);
-  } else {
-    return (
-      <SidebarProvider defaultOpen={false}>
-        <RentaCalSidebar />
-        <div className="w-screen">
-          <Header />
-          <SimulationResult />
-          <CookieConsent variant="small" />
-          <Footer />
-        </div>
-      </SidebarProvider>
-    );
-  }
+  return (
+    <SidebarProvider defaultOpen={false}>
+      <RentaCalSidebar />
+      <div className="w-screen">
+        <Header />
+        <SimulationResult />
+        <CookieConsent variant="small" />
+        <Footer />
+      </div>
+    </SidebarProvider>
+  );
 }
--- src/features/landing/HeroSection.tsx
+++ src/features/landing/HeroSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { signInAction } from "../auth/auth.action";
+import { redirect } from "next/navigation";
 
 export const HeroSection = () => {
   return (
@@ -25,7 +25,7 @@ export const HeroSection = () => {
             <Link
               href="#"
               onClick={() => {
-                signInAction();
+                redirect(`/simulateur`);
               }}
               className="py-3 px-6 text-center rounded-full duration-150 text-white text-semibold text-lg bg-blue-900 mb-5 hover:bg-blue-800 hover:ring ring-transparent ring-offset-2 transition"
             >
