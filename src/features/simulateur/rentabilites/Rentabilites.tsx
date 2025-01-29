3fa73afd32ba30a3294e8f40926a863aba717807
Guillaume <guillaume.rot@gmail.com>
2025-01-29 23:32:26 +0100

    feat: charts rentabilites

--- src/features/simulateur/rentabilites/Rentabilites.tsx
+++ src/features/simulateur/rentabilites/Rentabilites.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 import { PiSealPercentLight } from "react-icons/pi";
+import { RadialBar, RadialBarChart } from "recharts";
 
 export type CardRentabiliteProps = {
   label: string;
@@ -8,16 +9,50 @@ export type CardRentabiliteProps = {
 };
 
 export const CardRentabilite = (props: CardRentabiliteProps) => {
+  const data = [
+    {
+      name: "18-24",
+      uv: 31.47,
+      pv: 2400,
+      fill: "#8884d8",
+    },
+    {
+      name: "unknown",
+      uv: 6.67,
+      pv: 4800,
+      fill: "#ffc658",
+    },
+  ];
+
   return (
-    <Card className="rounded-3xl my-6 mx-14">
+    <Card className="rounded-3xl my-6 mx-10">
       <CardContent className="py-2 px-4">
-        <div className="p-4 flex flex-raw space-x-10">
+        <div className="p-4 flex flex-raw space-x-4">
           <div className="flex items-center">
             <PiSealPercentLight size={50} />
           </div>
           <div className="space-y-3">
             <h1 className="text-lg">{props.label}</h1>
             <div className="text-3xl font-medium">{props.pourcentage} %</div>
+          </div>
+          <div>
+            <RadialBarChart
+              width={150}
+              height={70}
+              cx={80}
+              cy={70}
+              innerRadius={40}
+              outerRadius={80}
+              barSize={30}
+              data={data}
+              endAngle={180}
+            >
+              <RadialBar
+                label={{ position: "insideStart", fill: "#fff" }}
+                background
+                dataKey="uv"
+              />
+            </RadialBarChart>
           </div>
         </div>
       </CardContent>
