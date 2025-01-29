cc49115a2b708f295a6065716dec3b99e6fb2677
Guillaume <guillaume.rot@gmail.com>
2025-01-29 09:24:27 +0100

    fix: layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,9 @@
 import { twx } from "@/lib/twx";
 
+export const Layout = twx.div((props) => [
+  `max-w-5xl w-full flex-col pb-6 flex gap-4 mx-auto px-4 font-poppins`,
+]);
+
 export const LayoutResult = twx.div((props) => [
   `flex flex-col lg:flex-row max-w-6xl w-full`,
 ]);
