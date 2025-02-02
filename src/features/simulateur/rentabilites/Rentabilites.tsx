259e6ae3c3ef1411b0cdec8dac50ee8e3f36a78b
Guillaume <guillaume.rot@gmail.com>
2025-02-02 12:49:03 +0100

    feat: rentabilites

--- src/features/simulateur/rentabilites/Rentabilites.tsx
+++ src/features/simulateur/rentabilites/Rentabilites.tsx
@@ -1,58 +1,65 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import React from "react";
 import { PiSealPercentLight } from "react-icons/pi";
-import { RadialBar, RadialBarChart } from "recharts";
 
 export type CardRentabiliteProps = {
   label: string;
   pourcentage: string;
+  description: string;
+  risk: string;
 };
 
 export const CardRentabilite = (props: CardRentabiliteProps) => {
-  const data = [
-    {
-      name: "18-24",
-      uv: 31.47,
-      pv: 2400,
-      fill: "#8884d8",
-    },
-    {
-      name: "unknown",
-      uv: 6.67,
-      pv: 4800,
-      fill: "#ffc658",
-    },
-  ];
+  var bgColor = "bg-green-200";
+  var textColor = "text-green-600";
+  if (props.risk == "middle") {
+    var bgColor = "bg-orange-200";
+    var textColor = "text-orange-600";
+  } else if (props.risk == "high") {
+    var bgColor = "bg-red-200";
+    var textColor = "text-red-600";
+  }
 
   return (
-    <Card className="rounded-3xl my-6 mx-10">
-      <CardContent className="py-2 px-4">
-        <div className="p-4 flex flex-raw space-x-4">
-          <div className="flex items-center">
-            <PiSealPercentLight size={50} />
-          </div>
-          <div className="space-y-3">
-            <h1 className="text-lg">{props.label}</h1>
-            <div className="text-3xl font-medium">{props.pourcentage} %</div>
+    <Card className="rounded-3xl my-6 mx-5">
+      <CardContent className="pt-6 pb-3 px-8">
+        <div className="flex flex-col space-y-2">
+          <div className="flex">
+            <PiSealPercentLight size={25} />
+            <div className="flex items-center">
+              <h1 className="ml-2 text-sm font-medium">{props.label}</h1>
+            </div>
           </div>
           <div>
-            <RadialBarChart
-              width={150}
-              height={70}
-              cx={80}
-              cy={70}
-              innerRadius={40}
-              outerRadius={80}
-              barSize={30}
-              data={data}
-              endAngle={180}
+            <Badge
+              className={`text-3xl font-medium rounded-3xl px-4 mt-1 hover:${bgColor} ${bgColor} ${textColor}`}
             >
-              <RadialBar
-                label={{ position: "insideStart", fill: "#fff" }}
-                background
-                dataKey="uv"
-              />
-            </RadialBarChart>
+              {props.pourcentage} %
+            </Badge>
+          </div>
+          <div className="text-xs font-medium text-gray-400 px-4">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem value="item-1" className="border-none">
+                <AccordionTrigger className="py-2">
+                  En savoir plus...
+                </AccordionTrigger>
+                <AccordionContent>
+                  {props.description}
+                  <p className="mt-2">
+                    Une rentabilité est considérée comme faible si elle est
+                    inférieure à 4%, moyenne si elle est entre 4% et 7%, et
+                    haute si elle est supérieure à 7%.
+                  </p>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
           </div>
         </div>
       </CardContent>
@@ -63,9 +70,28 @@ export const CardRentabilite = (props: CardRentabiliteProps) => {
 export class Rentabilites extends React.Component {
   render() {
     return (
-      <Card className="rounded-3xl w-full border-2 grid grid-cols-2">
-        <CardRentabilite label="Rentabilité brute" pourcentage="6,58" />
-        <CardRentabilite label="Rentabilité nette" pourcentage="6,49" />
+      <Card className="rounded-3xl w-full border-2 grid grid-cols-3 bg-blue-50 mb-100">
+        <CardRentabilite
+          label="Rentabilité brute"
+          pourcentage="6,58"
+          risk="low"
+          description="La rentabilité brute résulte de la division entre le prix d'achat et
+          les frais annexes dü à l'achat du bien par les revenus annuel."
+        />
+        <CardRentabilite
+          label="Rentabilité nette"
+          pourcentage="6,49"
+          risk="middle"
+          description="La rentabilité nette résulte de la division entre le prix d'achat et
+        les frais annexes dü à l'achat du bien par les revenus annuel."
+        />
+        <CardRentabilite
+          label="Rentabilité nette-nette"
+          pourcentage="5,38"
+          risk="high"
+          description="La rentabilité nette-nette résulte de la division entre le prix d'achat et
+        les frais annexes dü à l'achat du bien par les revenus annuel."
+        />
       </Card>
     );
   }
