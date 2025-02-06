62b26919347f7de7a743d674a925b3c6b5b6da68
Guillaume <guillaume.rot@gmail.com>
2025-02-06 21:55:34 +0100

    feat: wip graph

--- src/features/simulateur/amortissement/GraphAmortissement.tsx
+++ src/features/simulateur/amortissement/GraphAmortissement.tsx
@@ -12,36 +12,36 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 const chartData = [
-  { date: "2024-06-01", desktop: 178, mobile: 200 },
-  { date: "2024-06-02", desktop: 470, mobile: 410 },
-  { date: "2024-06-03", desktop: 103, mobile: 160 },
-  { date: "2024-06-04", desktop: 439, mobile: 380 },
-  { date: "2024-06-05", desktop: 88, mobile: 140 },
-  { date: "2024-06-06", desktop: 294, mobile: 250 },
-  { date: "2024-06-07", desktop: 323, mobile: 370 },
-  { date: "2024-06-08", desktop: 385, mobile: 320 },
-  { date: "2024-06-09", desktop: 438, mobile: 480 },
-  { date: "2024-06-10", desktop: 155, mobile: 200 },
-  { date: "2024-06-11", desktop: 92, mobile: 150 },
-  { date: "2024-06-12", desktop: 492, mobile: 420 },
-  { date: "2024-06-13", desktop: 81, mobile: 130 },
-  { date: "2024-06-14", desktop: 426, mobile: 380 },
-  { date: "2024-06-15", desktop: 307, mobile: 350 },
-  { date: "2024-06-16", desktop: 371, mobile: 310 },
-  { date: "2024-06-17", desktop: 475, mobile: 520 },
-  { date: "2024-06-18", desktop: 107, mobile: 170 },
-  { date: "2024-06-19", desktop: 341, mobile: 290 },
-  { date: "2024-06-20", desktop: 408, mobile: 450 },
-  { date: "2024-06-21", desktop: 169, mobile: 210 },
-  { date: "2024-06-22", desktop: 317, mobile: 270 },
-  { date: "2024-06-23", desktop: 480, mobile: 530 },
-  { date: "2024-06-24", desktop: 132, mobile: 180 },
-  { date: "2024-06-25", desktop: 141, mobile: 190 },
-  { date: "2024-06-26", desktop: 434, mobile: 380 },
-  { date: "2024-06-27", desktop: 448, mobile: 490 },
-  { date: "2024-06-28", desktop: 149, mobile: 200 },
-  { date: "2024-06-29", desktop: 103, mobile: 160 },
-  { date: "2024-06-30", desktop: 446, mobile: 400 },
+  { annee: "1", desktop: 178, mobile: 200 },
+  { annee: "2", desktop: 470, mobile: 410 },
+  { annee: "3", desktop: 103, mobile: 160 },
+  { annee: "4", desktop: 439, mobile: 380 },
+  { annee: "5", desktop: 88, mobile: 140 },
+  { annee: "6", desktop: 294, mobile: 250 },
+  { annee: "7", desktop: 323, mobile: 370 },
+  { annee: "8", desktop: 385, mobile: 320 },
+  { annee: "9", desktop: 438, mobile: 480 },
+  { annee: "10", desktop: 155, mobile: 200 },
+  { annee: "11", desktop: 92, mobile: 150 },
+  { annee: "12", desktop: 492, mobile: 420 },
+  { annee: "13", desktop: 81, mobile: 130 },
+  { annee: "14", desktop: 426, mobile: 380 },
+  { annee: "15", desktop: 307, mobile: 350 },
+  { annee: "16", desktop: 371, mobile: 310 },
+  { annee: "17", desktop: 475, mobile: 520 },
+  { annee: "18", desktop: 107, mobile: 170 },
+  { annee: "19", desktop: 341, mobile: 290 },
+  { annee: "20", desktop: 408, mobile: 450 },
+  { annee: "21", desktop: 169, mobile: 210 },
+  { annee: "22", desktop: 317, mobile: 270 },
+  { annee: "23", desktop: 480, mobile: 530 },
+  { annee: "24", desktop: 132, mobile: 180 },
+  { annee: "25", desktop: 141, mobile: 190 },
+  { annee: "26", desktop: 434, mobile: 380 },
+  { annee: "27", desktop: 448, mobile: 490 },
+  { annee: "28", desktop: 149, mobile: 200 },
+  { annee: "29", desktop: 103, mobile: 160 },
+  { annee: "30", desktop: 446, mobile: 400 },
 ];
 
 const chartConfig = {
@@ -59,15 +59,6 @@ const chartConfig = {
 } satisfies ChartConfig;
 
 export function Component() {
-  const filteredData = chartData.filter((item) => {
-    const date = new Date(item.date);
-    const referenceDate = new Date("2024-06-30");
-    let daysToSubtract = 30;
-    const startDate = new Date(referenceDate);
-    startDate.setDate(startDate.getDate() - daysToSubtract);
-    return date >= startDate;
-  });
-
   return (
     <Card className="m-5">
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
@@ -75,7 +66,7 @@ export function Component() {
           config={chartConfig}
           className="aspect-auto h-[250px] w-full"
         >
-          <AreaChart data={filteredData}>
+          <AreaChart data={chartData}>
             <defs>
               <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
                 <stop
@@ -104,17 +95,13 @@ export function Component() {
             </defs>
             <CartesianGrid vertical={false} />
             <XAxis
-              dataKey="date"
+              dataKey="annee"
               tickLine={false}
               axisLine={false}
               tickMargin={8}
               minTickGap={32}
               tickFormatter={(value) => {
-                const date = new Date(value);
-                return date.toLocaleDateString("en-US", {
-                  month: "short",
-                  day: "numeric",
-                });
+                return value + "e année";
               }}
             />
             <ChartTooltip
@@ -122,10 +109,7 @@ export function Component() {
               content={
                 <ChartTooltipContent
                   labelFormatter={(value) => {
-                    return new Date(value).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                    });
+                    return value + "e année";
                   }}
                   indicator="dot"
                 />
