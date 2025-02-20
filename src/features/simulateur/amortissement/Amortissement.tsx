3e112d594fb9adeaeb1aa829c3fa00ca9853b022
Guillaume <guillaume.rot@gmail.com>
2025-02-20 20:29:26 +0100

    feat: bar charts

--- src/features/simulateur/amortissement/Amortissement.tsx
+++ src/features/simulateur/amortissement/Amortissement.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/features/theme/CardUtils";
 import { BsGraphUpArrow } from "react-icons/bs";
 import { AmortissementType } from "../simulateur.schema";
-import { ComponentTest } from "./ComponentTest";
 import { GraphAmortissement } from "./GraphAmortissement";
 import { TabAmortissement } from "./TabAmortissement";
 
@@ -17,39 +16,6 @@ export type AmortissementProps = {
   resultatAnnuel: AmortissementType[];
   // resultatMensuel?: AmortissementType[];
 };
-
-// const chartData = [
-//   { annee: "1", interet: 250, pret: 260 },
-//   { annee: "2", interet: 240, pret: 270 },
-//   { annee: "3", interet: 230, pret: 280 },
-//   { annee: "4", interet: 220, pret: 290 },
-//   { annee: "5", interet: 210, pret: 300 },
-//   { annee: "6", interet: 200, pret: 310 },
-//   { annee: "7", interet: 190, pret: 320 },
-//   { annee: "8", interet: 180, pret: 330 },
-//   { annee: "9", interet: 170, pret: 340 },
-//   { annee: "10", interet: 160, pret: 350 },
-//   { annee: "11", interet: 150, pret: 360 },
-//   { annee: "12", interet: 140, pret: 370 },
-//   { annee: "13", interet: 130, pret: 380 },
-//   { annee: "14", interet: 120, pret: 390 },
-//   { annee: "15", interet: 110, pret: 400 },
-//   { annee: "16", interet: 100, pret: 410 },
-//   { annee: "17", interet: 90, pret: 420 },
-//   { annee: "18", interet: 80, pret: 430 },
-//   { annee: "19", interet: 70, pret: 440 },
-//   { annee: "20", interet: 60, pret: 450 },
-//   { annee: "21", interet: 50, pret: 460 },
-//   { annee: "22", interet: 40, pret: 470 },
-//   { annee: "23", interet: 35, pret: 475 },
-//   { annee: "24", interet: 30, pret: 480 },
-//   { annee: "25", interet: 25, pret: 485 },
-//   { annee: "26", interet: 20, pret: 490 },
-//   { annee: "27", interet: 18, pret: 492 },
-//   { annee: "28", interet: 15, pret: 495 },
-//   { annee: "29", interet: 5, pret: 505 },
-//   { annee: "30", interet: 1, pret: 509 },
-// ];
 
 export const Amortissement = (props: AmortissementProps) => {
   return (
@@ -62,7 +28,6 @@ export const Amortissement = (props: AmortissementProps) => {
         <CardContent className="flex-1 pb-0">
           <GraphAmortissement data={props.resultatAnnuel} />
           <TabAmortissement />
-          <ComponentTest />
           <div className="text-xs font-medium text-gray-400 w-full px-4 lg:px-10 mt-4">
             <AccordionSeeMore
               description="Une rentabilité est considérée comme faible si elle est
--- src/features/simulateur/amortissement/GraphAmortissement.tsx
+++ src/features/simulateur/amortissement/GraphAmortissement.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
 
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -21,24 +21,54 @@ export type DataProps = {
 };
 
 const chartData = [
-  { annee: "1", mensualitesAnnuelles: 3, cashflowNetNet: 2 },
-  { annee: "2", mensualitesAnnuelles: 3, cashflowNetNet: 2 },
-  { annee: "3", mensualitesAnnuelles: 3, cashflowNetNet: 2 },
-  { annee: "4", mensualitesAnnuelles: 1, cashflowNetNet: 4 },
-  { annee: "5", mensualitesAnnuelles: 1, cashflowNetNet: 4 },
+  { annee: "1", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "2", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "3", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "4", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "5", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "6", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "7", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "8", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "9", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "10", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "11", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "12", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "13", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "14", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "15", pret: 300, ps: 50, ir: 50, cashflow: 100 },
+  { annee: "16", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "17", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "18", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "19", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "20", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "21", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "22", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "23", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "24", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "25", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "26", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "27", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "28", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "29", pret: 0, ps: 100, ir: 100, cashflow: 300 },
+  { annee: "30", pret: 0, ps: 100, ir: 100, cashflow: 300 },
 ];
 
 const chartConfig = {
-  visitors: {
-    label: "Visitors",
+  pret: {
+    label: "Crédit",
+    color: "hsl(var(--chart-1))",
   },
-  cashflowNetNet: {
+  ps: {
+    label: "Prel. Soc.",
+    color: "hsl(var(--chart-5))",
+  },
+  ir: {
+    label: "Imp. Rev",
+    color: "hsl(var(--chart-4))",
+  },
+  cashflow: {
     label: "Cashflow",
     color: "hsl(var(--chart-2))",
-  },
-  mensualitesAnnuelles: {
-    label: "Mensualites",
-    color: "hsl(var(--chart-1))",
   },
 } satisfies ChartConfig;
 
@@ -46,38 +76,8 @@ export function GraphAmortissement(props: DataProps) {
   return (
     <Card className="m-5">
       <CardContent className="px-2 pt-4 sm:px-6 sm:pt-6">
-        <ChartContainer
-          config={chartConfig}
-          className="aspect-auto h-[250px] w-full"
-        >
-          <AreaChart data={props.data}>
-            {/* <AreaChart data={chartData}> */}
-            <defs>
-              <linearGradient id="fillCashflow" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-cashflowNetNet)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-cashflowNetNet)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-              <linearGradient id="fillMensualites" x1="0" y1="0" x2="0" y2="1">
-                <stop
-                  offset="5%"
-                  stopColor="var(--color-mensualitesAnnuelles)"
-                  stopOpacity={0.8}
-                />
-                <stop
-                  offset="95%"
-                  stopColor="var(--color-mensualitesAnnuelles)"
-                  stopOpacity={0.1}
-                />
-              </linearGradient>
-            </defs>
+        <ChartContainer config={chartConfig} className="max-h-[350px] w-full">
+          <BarChart accessibilityLayer data={chartData}>
             <CartesianGrid vertical={false} />
             <XAxis
               dataKey="annee"
@@ -89,34 +89,33 @@ export function GraphAmortissement(props: DataProps) {
                 return value + "e année";
               }}
             />
-            <ChartTooltip
-              cursor={false}
-              content={
-                <ChartTooltipContent
-                  labelFormatter={(value) => {
-                    return value + "e année";
-                  }}
-                  indicator="dot"
-                />
-              }
-            />
-            <Area
-              dataKey="mensualitesAnnuelles"
-              type="natural"
-              fill="url(#fillMensualites)"
-              stroke="var(--color-mensualitesAnnuelles)"
-              stackId="a"
-            />
-            <Area
-              dataKey="cashflowNetNet"
-              type="natural"
-              fill="url(#fillCashflow)"
-              stroke="var(--color-cashflowNetNet)"
-              stackId="a"
-            />
-
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
             <ChartLegend content={<ChartLegendContent />} />
-          </AreaChart>
+            <Bar
+              dataKey="pret"
+              stackId="a"
+              fill="var(--color-pret)"
+              radius={[0, 0, 4, 4]}
+            />
+            <Bar
+              dataKey="ps"
+              stackId="a"
+              fill="var(--color-ps)"
+              radius={[0, 0, 0, 0]}
+            />
+            <Bar
+              dataKey="ir"
+              stackId="a"
+              fill="var(--color-ir)"
+              radius={[0, 0, 0, 0]}
+            />
+            <Bar
+              dataKey="cashflow"
+              stackId="a"
+              fill="var(--color-cashflow)"
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
         </ChartContainer>
       </CardContent>
     </Card>
