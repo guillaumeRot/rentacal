15c1fa882d261555b0cd31560b489f12bbd6f9ab
Guillaume <guillaume.rot@gmail.com>
2025-02-26 13:19:06 +0100

    feat: accordion wip

--- src/features/simulateur/amortissement/TabAmortissement.tsx
+++ src/features/simulateur/amortissement/TabAmortissement.tsx
@@ -6,6 +6,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { MontantFormat } from "@/features/MontantFormat";
 
@@ -80,71 +81,85 @@ export function TabAmortissement() {
               </AccordionTrigger>
 
               <AccordionContent>
-                <div>
-                  <span className="text-center py-2">Loyers annuels</span>
-                  <span className="text-center py-2">
-                    {resultat.loyersAnnuel}
-                  </span>
-                  <span className="text-center py-2">Sur</span>
-                  <span className="text-center py-2">
-                    {resultat.vacanceLocative}
-                  </span>
-                </div>
-                <div className="flex-1 pb-0 grid grid-cols-1 lg:grid-cols-2 items-center">
-                  <Card className="rounded-3xl p-4 lg:p-10 m-4 grid grid-cols-2 h-fit text-sm lg:text-md">
-                    {/* TODO: Faire un pie chart */}
-                    <span className="text-center py-2">
-                      Amortissement immobilier
-                    </span>
-                    <span className="text-center py-2">
-                      {resultat.amortissementImmo}
-                    </span>
-                    <span className="text-center py-2">
-                      Amortissement travaux
-                    </span>
-                    <span className="text-center py-2">
-                      {resultat.amortissementTravaux}
-                    </span>
-                    <span className="text-center py-2">
-                      Amortissement mobilier
-                    </span>
-                    <span className="text-center py-2">
-                      {resultat.amortissementMobilier}
-                    </span>
-                    <span className="text-center pb-2 pt-6 font-semibold">
-                      Amortissement total
-                    </span>
-                    <span className="text-center pb-2 pt-6 font-semibold">
-                      <MontantFormat
-                        value={
-                          resultat.amortissementImmo +
-                          resultat.amortissementTravaux +
-                          resultat.amortissementMobilier
-                        }
-                      />
-                    </span>
-                  </Card>
-                  <Card className="rounded-3xl p-4 lg:p-10 m-4 grid grid-cols-2 h-fit text-sm lg:text-md">
-                    <span className="text-center py-2">Crédit</span>
-                    <span className="text-center py-2">{resultat.pret}</span>
-                    <span className="text-center py-2">
-                      Impots sur le revenu
-                    </span>
-                    <span className="text-center py-2">{resultat.ir}</span>
-                    <span className="text-center py-2">
-                      Prélèvements sociaux
-                    </span>
-                    <span className="text-center py-2">{resultat.ps}</span>
-                    <span className="text-center pb-2 pt-6 font-semibold">
-                      Total des dépenses
-                    </span>
-                    <span className="text-center pb-2 pt-6 font-semibold">
-                      <MontantFormat
-                        value={resultat.pret + resultat.ir + resultat.ps}
-                      />
-                    </span>
-                  </Card>
-                </div>
+                <Card className="rounded-3xl p-0 h-fit bg-blue-50">
+                  <div className="text-center mt-6 mx-4 lg:mx-20 grid grid-cols-1 lg:grid-cols-2">
+                    <div className="flex flex-col px-4 lg:px-10">
+                      <span className="text-center text-md font-semibold">
+                        Loyers annuels
+                      </span>
+                      <div className="py-2">
+                        <Badge className="text-lg lg:text-xl font-medium rounded-3xl px-5 py-1 mt-1 hover:bg-green-200 bg-green-200 text-green-600 border border-green-600">
+                          <MontantFormat value={resultat.loyersAnnuel} />
+                        </Badge>
+                      </div>
+                    </div>
+                    <div className="flex flex-col">
+                      <span className="text-center text-md font-semibold">
+                        Répartis sur
+                      </span>
+                      <div className="py-2">
+                        <Badge className="text-lg lg:text-xl font-medium rounded-3xl px-5 py-1 mt-1 hover:bg-blue-50 bg-blue-50 text-blue-600 border border-blue-600">
+                          {resultat.vacanceLocative} mois
+                        </Badge>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 pb-0 grid grid-cols-1 lg:grid-cols-2 items-center">
+                    <Card className="rounded-3xl p-4 lg:p-10 m-4 grid grid-cols-2 h-fit text-sm lg:text-md">
+                      {/* TODO: Faire un pie chart */}
+                      <span className="text-center py-2">
+                        Amortissement immobilier
+                      </span>
+                      <span className="text-center py-2">
+                        {resultat.amortissementImmo}
+                      </span>
+                      <span className="text-center py-2">
+                        Amortissement travaux
+                      </span>
+                      <span className="text-center py-2">
+                        {resultat.amortissementTravaux}
+                      </span>
+                      <span className="text-center py-2">
+                        Amortissement mobilier
+                      </span>
+                      <span className="text-center py-2">
+                        {resultat.amortissementMobilier}
+                      </span>
+                      <span className="text-center pb-2 pt-6 font-semibold">
+                        Amortissement total
+                      </span>
+                      <span className="text-center pb-2 pt-6 font-semibold">
+                        <MontantFormat
+                          value={
+                            resultat.amortissementImmo +
+                            resultat.amortissementTravaux +
+                            resultat.amortissementMobilier
+                          }
+                        />
+                      </span>
+                    </Card>
+                    <Card className="rounded-3xl p-4 lg:p-10 m-4 grid grid-cols-2 h-fit text-sm lg:text-md">
+                      <span className="text-center py-2">Crédit</span>
+                      <span className="text-center py-2">{resultat.pret}</span>
+                      <span className="text-center py-2">
+                        Impots sur le revenu
+                      </span>
+                      <span className="text-center py-2">{resultat.ir}</span>
+                      <span className="text-center py-2">
+                        Prélèvements sociaux
+                      </span>
+                      <span className="text-center py-2">{resultat.ps}</span>
+                      <span className="text-center pb-2 pt-6 font-semibold">
+                        Total des dépenses
+                      </span>
+                      <span className="text-center pb-2 pt-6 font-semibold">
+                        <MontantFormat
+                          value={resultat.pret + resultat.ir + resultat.ps}
+                        />
+                      </span>
+                    </Card>
+                  </div>
+                </Card>
                 {/* <Table>
                   <TableHeader>
                     <TableRow>
